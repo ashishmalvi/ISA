8bd33e37d72ca3157d369ca681238b7ed3c90d01
Sumeet Sharma <sumeet.sharma@berkeley.edu>
2012-09-29 02:47:41 -0700

    lol

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ var fs = require('fs')
 var qs = require('querystring')
 var httpreq = require('request')
 var index = fs.readFileSync(__dirname + '/index.ejs', 'utf8');
+var about = fs.readFileSync(__dirname + '/about.ejs', 'utf8');
+var membership = fs.readFileSync(__dirname + '/membership.ejs', 'utf8');
+var contact = fs.readFileSync(__dirname + '/contact.ejs', 'utf8');
 app.use(express.cookieParser());
 
 //index http request 
@@ -14,6 +17,15 @@ app.get('/', function (req, res) {
         res.send(ejs.render(index,{}));
 });
 
+app.get('/about', function (req, res) {
+        res.send(ejs.render(about,{}));
+});
+app.get('/membership', function (req, res) {
+        res.send(ejs.render(membership,{}));
+});
+app.get('/contact', function (req, res) {
+        res.send(ejs.render(contact,{}));
+});
 app.use(express.static(__dirname));
 app.use(express.static(__dirname + '/bootstrap'));
 //process.env.PORT will be set when the heroku site initializes
